80577b2da1f313768f68aed63e715089f11fa216
Gustavo.M <Enderdragon3245@gmail.com>
2024-04-03 16:24:14 -0300

    Feat: Implementing CheckIn Repository

---
+++ src/models/entities/check_ins.py
@@ -0,0 +1,17 @@
+from src.models.settings.base import Base
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from sqlalchemy.sql import func
+
+class CheckIns(Base):
+    __tablename__ = "check_ins"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    created_at = Column(DateTime, default=func.now())
+    attendeeId = Column(String, ForeignKey("attendees.id"))
+    
+ 
+    
+
+    def __repr__(self):
+        return f"CheckIns [attendeeId={self.attendeeId}]"
+    
