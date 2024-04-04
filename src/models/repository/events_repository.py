03b5c4769cf410cd3ce4836f0d32d4e3c8e5d4f8
Gustavo.M <Enderdragon3245@gmail.com>
2024-04-04 13:56:33 -0300

    feat: Implementing get event route

--- src/models/repository/events_repository.py
+++ src/models/repository/events_repository.py
@@ -1,6 +1,7 @@
 from typing import Dict
 from src.models.settings.connection import db_connection_handler
 from src.models.entities.events import Events
+from src.models.entities.attendees import Attendees
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.exc import NoResultFound
 
@@ -38,5 +39,31 @@ class EventsRepository:
                 return event
             except NoResultFound:
                 return None
-
-#Primeira Modificação <--->
+    
+    def count_event_attendees(self, event_id: str) -> Dict:
+         with db_connection_handler as database:
+            try:
+                event_count = (
+                    database.session
+                        .query(Events)
+                        .join(Attendees, Events.id == Attendees.event_id)
+                        .filter(Events.id == event_id)
+                        .with_entities(
+                            Events.maximum_attendees,
+                            Attendees.id
+                        )
+                        .all()
+                 )
+                if not len(event_count):
+                    return {
+                        "maximumAttendees": 0,
+                        "attendeesAmount": 0,
+                    }
+                 
+                return {
+                        "maximumAttendees": event_count[0].maximum_attendees,
+                        "attendeesAmount": len(event_count),
+                }
+         
+            except:
+                pass
