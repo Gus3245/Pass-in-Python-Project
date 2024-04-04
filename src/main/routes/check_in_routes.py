a233fdb4caf5cfb3a10d9f914df70f955c0ee988
Gustavo.M <Enderdragon3245@gmail.com>
2024-04-04 16:18:10 -0300

    feat: implementig Check_in_routes

---
+++ src/main/routes/check_in_routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, jsonify, request
+from src.http_types.http_request import HttpRequest
+from src.data.check_in_handler import CheckInHandler
+
+check_in_route_bp = Blueprint("check_in_route", __name__)
+
+@check_in_route_bp.route("/attendees/<attendee_id>/check-in", methods=["POST"])
+def create_check_in(attendee_id):
+    check_in_handler = CheckInHandler()
+    Http_request = HttpRequest(param={"attendee_id": attendee_id })
+    http_response = check_in_handler.registry(Http_request)
+
+    return jsonify(http_response.body), http_response.status_code
