50167a80c6907231638be0033f9e1b69ae87696f
Gustavo.M <Enderdragon3245@gmail.com>
2024-04-03 22:43:58 -0300

    feat: integrating Event-Handler And http_Types

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
+from src.models.settings.connection import db_connection_handler
+
+db_connection_handler.connect_to_db()
 
 app = Flask(__name__)
 CORS(app)
